efaa86f1690e00fd54cd5df022775adc3b9698a7
Gakthux <charlie.henin@epitech.eu>
2015-11-09 08:49:47 +0100

    remove shit

--- web-app/api-server-backup/config.js
+++
@@ -1,4 +0,0 @@
-exports.config = {
-	envioApiAccessKey: "f8c5e1xx5f48e56s4x8",
-    appName: 'api-service'
-};
--- web-app/web-app-backup/server/config.js
+++
@@ -1,7 +0,0 @@
-// Configuration variables with default values for development mode
-
-exports.config = {
-	envioApiAccessKey: "f8c5e1xx5f48e56s4x8",
-	apiAddress: "http://localhost:1337/api",
-	webServerHttpPort: 8080
-};
