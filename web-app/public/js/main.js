225c08aee437bf99b90ccb6063793972ad5b54af
simus81 <simon.torradou@gmail.com>
2015-11-17 07:32:35 +0100

    Revert "Room fonction"

--- web-app/public/js/main.js
+++ web-app/public/js/main.js
@@ -19,6 +19,7 @@ var Header = React.createClass({
 var Sidemenu = React.createClass({
     render() {
         return (
+
             <ul className="table-view">
               <li className="table-view-cell media">
                 <a className="navigate-right">
@@ -37,7 +38,12 @@ var Sidemenu = React.createClass({
                 </a>
               </li>
               <li className="table-view-cell media">
-                    <Link to="/Rooms">Rooms</Link>
+                <a className="navigate-right">
+                  <span className="media-object pull-left icon icon-pages"></span>
+                  <div className="media-body">
+                    Item 3
+                  </div>
+                </a>
               </li>
             </ul>  
         );
@@ -45,216 +51,18 @@ var Sidemenu = React.createClass({
 });
 
 var Home = React.createClass({
-      handleClick: function(event) {
-      react = this
-       HttpPost('/logout', {
-            'guid': cookie.load('userId')
-        }, function(ret) {
-            //rep = jQuery.parseJSON(ret)
-             if(ret.error == null){
-                react.props.doLogout()
-             }
-        })
-      },
-      render() {
-          /* HttpPost('/infoUserId', {
-          //     }, function(ret) {
-          //         console.log(ret)
-          //     })*/
-          return (
-              <div className="content">
-              <Sidemenu />
-                <ul className="table-view">
-                  <li className="table-view-cell media">
-                  boum
-                  </li>
-                </ul>
-                <button onClick={this.handleClick}>logout</button>
-              </div>
-          );
-      }
-});
-
-
-var RoomListItem = React.createClass({
-    render: function () {
-            return (
-            <li className="table-view-cell media">
-            <div>name: {this.props.room.name}</div>
-            <div>Temperature actuel: {this.props.room.realTemperature}</div>
-            <div>Temperature voulus: {this.props.room.temperature}</div>
-            <div>m²: {this.props.room.volume}</div>
-            <Link to={`/Rooms/${this.props.room._id}`}>Modifier</Link>
-            </li>
-        );
-    }
-});
-
-var RoomList = React.createClass({
-    render: function () {
-        var items = this.props.rooms.map(function (room) {
-            return (
-                <RoomListItem key={room.id} room={room} />
-            );
-        });
-        return (
-            <ul  className="table-view">
-                {items}
-            </ul>
-        );
-    }
-});
-
-var Room = React.createClass({
-      getInitialState: function() {        
-        return {
-            rooms: []
-        }
-      },
-      componentDidMount: function() {
-        react = this
-          HttpPost('/getRooms', {
-            'organisation': 'Envio',// a changer avec les info users            
-        }, function(ret) {         
-            rep = jQuery.parseJSON(ret)
-            if (rep.error == null){              
-              react.setState({rooms: rep.rooms})
-            }
-            else{
-              react.setState({rooms: rep.error})
-            }            
-        })
-      },
-      render() {
-          return (
-              <div>
-                <Header text="Envio Room"/>
-                <div className="content">
-                    <RoomList rooms={this.state.rooms}/>
-                    <Link to="/CreatRoom">Creat room</Link>
-                </div>
-              </div>
-          );
-      }
-});
-
-var ModifRoom = React.createClass({
-    getInitialState: function() {
-        return {
-            status: false,
-            room: null
-        }
-    },
-    componentDidMount: function() {
-       react = this;
-       HttpPost('/getRoom', {
-          'roomID': react.props.params.Id,         
-      }, function(ret) {          
-          rep = jQuery.parseJSON(ret)
-          console.log(rep)
-          react.setState({room: rep.room})
-      })
-    },    
-    handleSubmit(event) {
-        event.preventDefault()
-        var name = ReactDOM.findDOMNode(this.refs.name).value
-        var volume = ReactDOM.findDOMNode(this.refs.volume).value
-        react = this;
-        HttpPost('/modifyRoom', {
-            'newName': name,
-            'name': react.state.room.name,
-            'volume': volume,            
-        }, function(ret) {          
-            rep = jQuery.parseJSON(ret)
-            console.log(rep)
-            react.setState({status: rep})
-        })
-    },
-    ChangeTemp(event) {
-        var newValue = ReactDOM.findDOMNode(this.refs.temperature).value
-        react = this;
-        HttpPost('/changeTemperature', {
-            'roomID': react.props.params.Id,
-            'temperature': newValue,
-        }, function(ret) {          
-            rep = jQuery.parseJSON(ret)
-            console.log(rep)
-            react.setState({status: rep})
-        })
-    },
     render() {
-        if(this.state.status){
-          if (this.state.status.error == null) 
-          {            
-            return (
-                    <div className="bar bar-header-secondary">
-                        Modif success full!
-                    </div>
-                   );                    
-          }
-        }      
+        /* HttpPost('/infoUser', {
+        //     }, function(ret) {
+        //         console.log(ret)
+        //     })*/
         return (
-        <div className="bar bar-header-secondary">
-             <form role="form" onSubmit={this.handleSubmit}>
-                 <div className="form-group">
-                  <input ref="name" type="text" placeholder="new name" />
-                  <input ref="volume" type="text" placeholder="new volume" />
-                </div>
-                <button type="submit" >modif</button>
-              </form>
-              <input ref="temperature" type="number" placeholder={this.state.room ? this.state.room.temperature : 5} />
-              <button onClick={this.ChangeTemp} >change temperature</button>
-        </div>
-        );
-    }
-});
-
-
-var CreatRoom = React.createClass({
-    getInitialState: function() {
-        return {
-            status: false,
-        }
-    },
-    handleSubmit(event) {
-        event.preventDefault()
-        var organisation = ReactDOM.findDOMNode(this.refs.organisation).value
-        var name = ReactDOM.findDOMNode(this.refs.name).value
-        var volume = ReactDOM.findDOMNode(this.refs.volume).value
-        react = this;
-        HttpPost('/createRoom', {
-            'organisation': organisation,
-            'name': name,
-            'volume': volume,
-            
-        }, function(ret) {          
-            rep = jQuery.parseJSON(ret)
-            console.log(rep)
-            react.setState({status: rep})
-        })
-    },
-    render() {
-        if(this.state.status){
-          if (this.state.status.error == null) 
-          {            
-            return (
-                    <div className="bar bar-header-secondary">
-                        Creat success full!
-                    </div>
-                   );                    
-          }
-        }      
-        return (
-        <div className="bar bar-header-secondary">
-             <form role="form" onSubmit={this.handleSubmit}>
-                 <div className="form-group">
-                  <input ref="organisation" type="text" placeholder="organisation" />
-                  <input ref="name" type="text" placeholder="name" />
-                  <input ref="volume" type="text" placeholder="volume" />
-                </div>
-                <button type="submit" >Creat</button>
-              </form>
-        </div>
+            <div className="content">
+            <Sidemenu />
+                      <ul className="table-view">
+              <li className="table-view-cell media">
+              boum</li></ul>
+            </div>
         );
     }
 });
@@ -268,12 +76,10 @@ var Login = React.createClass({
         HttpPost('/login', {
             'email': email,
             'password': pass,
-        }, function(ret) {          
-            rep = jQuery.parseJSON(ret)
-            console.log(rep)
-             if(rep.error == null){
-              react.props.doLogin(rep.guid)            
-             }
+            'api_key' : "f8c5e1xx5f48e56s4x8",
+        }, function(ret) {
+            console.log(ret)
+            //react.props.doLogin(ret.name)
         })
     },
     render() {
@@ -286,7 +92,6 @@ var Login = React.createClass({
                 </div>
                 <button type="submit" >Submit</button>
               </form>
-              <Link to="/Register">Register</Link>
         </div>
         );
     }
@@ -295,7 +100,7 @@ var Login = React.createClass({
 const Register = React.createClass({
     getInitialState: function() {
         return {
-            registered: false,
+            loged: false,
         }
     },
     handleSubmit(event) {
@@ -304,44 +109,38 @@ const Register = React.createClass({
             var pass = ReactDOM.findDOMNode(this.refs.pass).value
             var firstname = ReactDOM.findDOMNode(this.refs.firstname).value
             var lastname = ReactDOM.findDOMNode(this.refs.lastname).value
-            var organisation = ReactDOM.findDOMNode(this.refs.organisation).value
             react = this;
+            console.log(this.state.loged)
             HttpPost('/register', {
                 'email': email,
                 'password': pass,
                 'firstname': firstname,
-                'lastname': lastname,
-                'organisation' : organisation            
+                'lastname': lastname            
             }, function(ret) {
-                react.setState({registered: ret})
+                //console.log(ret)
+                react.setState({loged: true})
             })
         },
         render() {
-            if(this.state.registered){
-              if (this.state.registered.error == null) 
-              {            
-                return (
-                        <div className="bar bar-header-secondary">
-                            register success full!
-                        </div>
-                       );                    
-              }
+            var loged
+            if(this.state.loged){
+            return (
+                    <div className="bar bar-header-secondary">
+                        <Home />
+                    </div>
+                    );                    
             }
             return (
-              <div className={"page " + this.props.position}>
-               <Header text="Envio intranet" back="false"/>
-                <div className="bar bar-header-secondary">
-                   <form role="form" onSubmit={this.handleSubmit}>
-                      <div className="form-group">
-                      <input ref="firstname" type="text" placeholder="firstname" />
-                      <input ref="lastname" type="text" placeholder="lastname" />
+            <div className="bar bar-header-secondary">
+                 <form role="form" onSubmit={this.handleSubmit}>
+                     <div className="form-group">
+                     <input ref="firstname" type="text" placeholder="firstname" />
+                     <input ref="lastname" type="text" placeholder="lastname" />
                       <input ref="email" type="text" placeholder="email" />
                       <input ref="pass" type="password" placeholder="Password" />
-                      <input ref="organisation" type="text" placeholder="Organisation" />                     
-                      </div>
-                      <button type="submit" >Submit</button>
-                    </form>
-                </div>
+                    </div>
+                    <button type="submit" >Submit</button>
+                  </form>
             </div>
             );
     }
@@ -358,22 +157,18 @@ const App = React.createClass({
         this.setState({userId : userId});
         cookie.save('userId', userId);
     },
-    doLogout: function() {
-        cookie.remove('userId');            
-        this.setState({userId: false});
-    },
     render() {
           if(this.state.userId){
             return (
             <div className={"page " + this.props.position}>
-                <Header text="Envio intranet" />
-                <Home  doLogout={this.doLogout}/>
+                <Header text="Envio intranet" back="false"/>
+                <Home />
             </div>
                     );                    
             }
         return (
             <div className={"page " + this.props.position}>
-                <Header text="Envio intranet"/>
+                <Header text="Envio intranet" back="false"/>
                 <Login doLogin={this.doLogin}/>
             </div>
         );
@@ -386,9 +181,6 @@ var Racine = React.createClass({
             <Router>
                 <Route path="/" component={App} />                
                 <Route path="/Register" component={Register}/>
-                <Route path="/CreatRoom" component={CreatRoom}/>
-                <Route path="/Rooms" component={Room}/>
-                <Route path="/Rooms/:Id" component={ModifRoom}/>
           </Router>
     )}
 })
