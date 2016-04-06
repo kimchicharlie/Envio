a848e3e0bf981b7001789d71b8fb074ae2a7abf7
Gakthux <charlie.henin@epitech.eu>
2016-04-06 13:34:25 +0900

    site vitrine au propre

---
+++ site-vitrine/js-plugin/toucheeffect/toucheffects.js
@@ -0,0 +1,21 @@
+/** Used Only For Touch Devices **/
+$(document).ready(function() { 
+
+	// for touch devices: add class cs-hover to the figures when touching the items
+	if( Modernizr.touch ) {
+
+		$( '.imgHover article:not(.generatedMoreLink)  figure').on( 'touchstart', function(e) {
+
+			$(this).find( '.iconLinks > a, a.btn' ).on( 'touchstart', function(e) {
+				e.stopPropagation();
+			});
+
+			$(this).toggleClass('cs-hover');
+
+		});
+
+
+
+	}
+
+});
