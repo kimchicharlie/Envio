7daf496094bec8f96ce18834d69dbd4dc6336429
simus81 <simon.torradou@gmail.com>
2016-07-02 15:04:46 +0200

    correction bug stat

--- web-app/public/js/mode.jsx
+++ web-app/public/js/mode.jsx
@@ -26,7 +26,6 @@ ModeListItem = React.createClass({
 			<li className="mode-elem">
 				<span className="w_20p">Nom : {this.props.mode.name}</span>
 				<span className="w_20p">Luminosité: {this.props.mode.light}</span>
-				<span className="w_20p">Opacité : {this.props.mode.opacity}</span>
 				<span className="w_20p">Température : {this.props.mode.temperature}</span>
 				<button className="list-button w_15p" onClick={this.ModifMode}>Modifier</button>
 				<button className="list-button w_15p" onClick={this.DeleteMode}>Supprimer</button>
@@ -166,14 +165,13 @@ ModifMode = React.createClass({
 		event.preventDefault();
 		var newName = ReactDOM.findDOMNode(this.refs.newName).value;
 		var light = ReactDOM.findDOMNode(this.refs.light).value;
-		var opacity = ReactDOM.findDOMNode(this.refs.opacity).value;
 		var temperature = ReactDOM.findDOMNode(this.refs.temperature).value;
 		react = this;
 		HttpPost('/modifyMode', {
 			'modeID' : react.props.Id,
 			'newName' : newName,
 			'light' : light,
-			'opacity' : opacity,
+			'opacity' : 0,
 			'temperature' : temperature
 		}, function(rep) {          
 			rep = jQuery.parseJSON(rep)
@@ -198,9 +196,6 @@ ModifMode = React.createClass({
 		            	<input className="input-medium" ref="light" type="text" placeholder="Luminosité"/>
 		            </div>
 		            <div className="input-container">
-		            	<input className="input-medium" ref="opacity" type="text" placeholder="Opacité"/>
-		            </div>
-		            <div className="input-container">
 		            	<input className="input-medium" ref="temperature" type="text" placeholder="Température"/>
 		            </div>
 				</div>
@@ -212,6 +207,10 @@ ModifMode = React.createClass({
 		);
 	}
 });
+
+		            // <div className="input-container">
+		            // 	<input className="input-medium" ref="opacity" type="text" placeholder="Opacité"/>
+		            // </div>
 
 
 CreateMode = React.createClass({
@@ -232,7 +231,7 @@ CreateMode = React.createClass({
 			'organisation': organisation,
 			'name': name,
 			'light': light,
-			'opacity': opacity,
+			'opacity': 0,
 			'temperature': temperature
 			
 		}, function(rep) {
@@ -261,9 +260,6 @@ CreateMode = React.createClass({
 		                	<input className="input-medium" ref="light" type="text" placeholder="Luminosité"/>
 		                </div>
 		                <div className="input-container">
-		                	<input className="input-medium" ref="opacity" type="text" placeholder="Opacité"/>
-		                </div>
-		                <div className="input-container">
 		                	<input className="input-medium" ref="temperature" type="text" placeholder="Température"/>
 		                </div>
 					</div>
@@ -275,6 +271,10 @@ CreateMode = React.createClass({
 		);
 	}
 });
+
+		                // <div className="input-container">
+		                // 	<input className="input-medium" ref="opacity" type="text" placeholder="Opacité"/>
+		                // </div>
 
 
 DeleteMode = React.createClass({
