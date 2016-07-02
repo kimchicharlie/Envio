b6db6d96021f784394aea37e7980a8ad8f4e3677
simus81 <simon.torradou@gmail.com>
2016-07-02 15:05:10 +0200

    correction bug stat

---
+++ web-app/config_local.js
@@ -0,0 +1,8 @@
+// Configuration variables with default values for development mode
+
+exports.config = {
+	envioApiAccessKey: "f8c5e1xx5f48e56s4x8",
+	//apiAddress: 'http://176.31.127.14:1337/api',
+	apiAddress: "http://localhost:1337/api",
+	webServerHttpPort: 8081
+};
