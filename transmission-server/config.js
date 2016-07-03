91920a62deaf0810ed6266588eaab4cf08fd00dc
Arkayl <pierre.hennebois@epitech.eu>
2016-07-03 16:36:41 +0200

    to pull

--- transmission-server/config.js
+++ transmission-server/config.js
@@ -4,7 +4,7 @@ exports.config = {
 	envioApiAccessKey: "f8c5e1xx5f48e56s4x8",
 	transmissionServerAccessKey: "r9c1a3r4v8d2z1x4e7m",
     appName: 'transmission-service',
-    roomID: '5777c96e7a3b6a0056f0fa92',
+    roomID: '57790a96111aadb946ab28a9',
     apiAddress: 'http://176.31.127.14:1337/api',
     //apiAddress: 'http://localhost:1337/api',
     simulatorAddress: 'http://localhost:8080/ModifyValue'
