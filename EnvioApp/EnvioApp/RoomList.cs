fd91b5e949caa9ffb279b620b8a17ecbc483df60
Arkayl <pierre.hennebois@epitech.eu>
2016-09-19 16:39:42 +0200

    Thermostat: modif fichier de config, pointe sur l'API en locale. Windows Phone app: Ajout du projet VS, fonctionnalités présentent: page de connexion, page principale avec selection de salle, sliders de température et de luminosité, page planning avec liste des modes pour la journée affichée, possibilité de modifier la journée affichée, page crédits avec bouton de déconnexion.

---
+++ EnvioApp/EnvioApp/RoomList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvioApp
+{
+    class RoomList
+    {
+        public List<Room> _rooms { get; set; }
+    }
+}
