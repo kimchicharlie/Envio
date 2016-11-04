b79ad45084d84924d96ac42a9f5558068bb94877
Gakthux <charlie.henin@epitech.eu>
2016-11-04 10:57:05 +0100

    dockerize services

--- web-app/public/js/ajaxQuery.js
+++ web-app/public/js/ajaxQuery.js
@@ -1,9 +1,8 @@
-// var baseURL = "http://localhost:8081/api";
-var baseURL = "http://137.74.40.245:8081/api";
+var baseURL = "http://localhost:8081/api";
+// var baseURL = "http://137.74.40.245:8081/api";
 
 function HttpPost(API, data, cb){
-    
-        $.ajax({
+    $.ajax({
         url: baseURL + API,                      
         type: 'POST',
         data: JSON.stringify(data),
