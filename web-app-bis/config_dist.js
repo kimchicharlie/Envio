1db65bef748cd6146f5d1d85b07981cde3edbbf1
Charlie <charlie.henin@gmail.com>
2017-01-13 17:43:06 +0100

    change url

--- web-app-bis/config_dist.js
+++ web-app-bis/config_dist.js
@@ -3,8 +3,8 @@
 exports.config = {
 	envioApiAccessKey: "f8c5e1xx5f48e56s4x8",
 	//apiAddress: 'http://176.31.127.14:8089/api',
-	// apiAddress: "http://137.74.40.245:8089/api",
-	apiAddress: "http://localhost:8089/api",
+	apiAddress: "http://137.74.40.245:8089/api",
+	// apiAddress: "http://localhost:8089/api",
 	webServerHttpPort: 8095,
 	isSQL: false
 };
